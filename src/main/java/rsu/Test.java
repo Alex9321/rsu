1825027fbcf1fb4858ac3ea62ccf5557070933e1
Pirvu Alexandru <alex@192-168-0-102.rdsnet.ro>
2016-05-28 12:03:18 +0300

    Some changes

--- src/main/java/rsu/Test.java
+++ src/main/java/rsu/Test.java
@@ -15,58 +15,66 @@ import java.sql.Statement;
 
 public class Test {
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
 
-        Class.forName("org.postgresql.Driver");
-        String url = "jdbc:postgresql://localhost:5432/osmosis";
-        PgConnection conn = (PgConnection) DriverManager.getConnection(url, "school", "Bunica21");
+		Class.forName("org.postgresql.Driver");
+		String url = "jdbc:postgresql://localhost:5432/zorilor";
+		PgConnection conn = (PgConnection) DriverManager.getConnection(url, "alex", "");
 
-        ServerSocket serverSocket = new ServerSocket(9999);
+		ServerSocket serverSocket = new ServerSocket(9999);
 
-        conn.addDataType("geometry", "org.postgis.PGgeometry");
-        Socket socket = serverSocket.accept();
-        BufferedReader inFromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        String streetName = "";
-        while (true) {
-            String position = inFromClient.readLine();
-            if (position == null) {
-                continue;
-            }
-//            System.out.println(position);
+		conn.addDataType("geometry", "org.postgis.PGgeometry");
+		Socket socket = serverSocket.accept();
+		BufferedReader inFromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		String streetName = "";
+		String firstPosition = null;
+		String secondPosition = null;
+		while (true) {
+			String position = inFromClient.readLine();
+			if (position == null) {
+				continue;
+			}
+			else {
+				if (firstPosition == null) {
+					firstPosition = position;
+					continue;
+				}
+				secondPosition = firstPosition;
+				firstPosition = position;
+			}
 
+			Statement s = conn.createStatement();
+			ResultSet r = s.executeQuery("select mapmatching(" + firstPosition + "," + secondPosition + ")");
 
-            Statement s = conn.createStatement();
-            ResultSet r = s.executeQuery("select mapmatching(" + position + ")");
+			r.next();
+			PGobject geom = (PGobject) r.getObject(1);
+			r.close();
+			if (geom.getValue().getBytes().length < 5) {
+				continue;
+			}
+			String success = geom.getValue().replace(")", "").split(",")[3];
+			String wayId = geom.getValue().replace(")", "").split(",")[2];
+			if (success.equals("1")) {
+				ResultSet way = s.executeQuery("select v from way_tags where k='name' and way_id='" + wayId + "'");
 
-//            while (r.next()) {
-//                PGobject geom = (PGobject) r.getObject(1);
-//                System.out.println(geom);
-//            }
-            r.next();
-            PGobject geom = (PGobject) r.getObject(1);
-            r.close();
-            if (geom.getValue().getBytes().length < 5) {
-                continue;
-            }
-            String wayId = geom.getValue().replace(")", "").split(",")[2];
-            ResultSet way = s.executeQuery("select v from way_tags where k='name' and way_id='" + wayId + "'");
+				way.next();
+				try {
+					String newStreet = way.getString(1);
+					if (!newStreet.equals(streetName)) {
+						streetName = newStreet;
+						System.out.println(streetName + " ----- at position:   ");
+						System.out.println(position);
+						System.out.println();
+					}
+				}
+				catch (Exception e) {
+					way.close();
+				}
+			}
 
-            way.next();
-            try {
-                String newStreet = way.getString(1);
-                if (!newStreet.equals(streetName)) {
-                    streetName = newStreet;
-                    System.out.println(streetName + " ----- at position:   ");
-                    System.out.println(position);
-                    System.out.println();
-                }
-            } catch (Exception e) {
-                way.close();
-            }
-
-            s.close();
-        }
+			s.close();
+		}
 //        conn.close();
-    }
+	}
 
 }
